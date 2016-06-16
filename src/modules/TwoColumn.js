9a4e1eace443eeb94b8992d1de182360f722f8e5
Jacqueline Wijaya <jackiewijaya@webmocha.com>
2016-06-15 17:19:14 -1000

    adding TwoColumn module (defaults to 50-50)
    Signed-off-by: Jackie Wijaya <jackiewijaya@webmocha.com>

---
+++ src/modules/TwoColumn.js
@@ -0,0 +1,76 @@
+// (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
+
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+import Box from 'grommet/components/Box';
+import Heading from 'grommet/components/Heading';
+
+const CLASS_ROOT = 'two-column';
+
+export default class TwoColumn extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  _renderHeader () {
+    const { headline, subHeadline } = this.props;
+
+    let headlineMarkup;
+    if (headline) {
+      headlineMarkup = (
+        <Heading tag="h2" strong={true} align="center">
+          {headline}
+        </Heading>
+      );
+    }
+
+    let subHeadlineMarkup;
+    if (subHeadline) {
+      subHeadlineMarkup = (
+        <Heading tag="h3" strong={true} align="center">
+          {subHeadline}
+        </Heading>
+      );
+    }
+
+    if (headlineMarkup || subHeadlineMarkup) {
+      return (
+        <Box pad="small">
+          {headlineMarkup}
+          {subHeadlineMarkup}
+        </Box>
+      );
+    }
+
+    return undefined;
+  }
+
+  render () {
+    const { mainColumn, children } = this.props;
+
+    const classes = classnames(
+      CLASS_ROOT,
+      {
+        [`${CLASS_ROOT}--50-50`]: !mainColumn,
+        [`${CLASS_ROOT}--33-66`]: mainColumn === 'end',
+        [`${CLASS_ROOT}--66-33`]: mainColumn === 'start'
+      },
+      this.props.className
+    );
+
+    return (
+      <Box className={classes} align="center" colorIndex="light-2" pad={{vertical: 'medium'}}>
+        {this._renderHeader()}
+        <Box className={`${CLASS_ROOT}__content`} direction="row">
+          {children}
+        </Box>
+      </Box>
+    );
+  }
+};
+
+TwoColumn.propTypes = {
+  headline: PropTypes.string,
+  subHeadline: PropTypes.string,
+  mainColumn: PropTypes.oneOf(['start', 'end'])
+};
